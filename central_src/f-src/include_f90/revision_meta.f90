933c8653922d3aede1228feef7d2e367881184e7
JoGebert <johannes.gebert@hlrs.de>
2022-02-23 10:53:11 +0100

    Subtree enhancement

--- central_src/f-src/include_f90/revision_meta.f90
+++ central_src/f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'Directly Discretizing Tensor Computation'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'ebc906d042fecbbaaa24378f4b1f6d34d5c23667'
+CHARACTER(LEN=scl), PARAMETER :: hash = '0142455330c458b5987534304630940fc3a96e86'
--- check_subtree.sh
+++ check_subtree.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 # ----------------------------------------------------------------------------------------
-# Johannes Gebert - Doctoral project - initialize the subtree of the centralized sources
+# Manage the state of a subtree
 #
-# Author:          Johannes Gebert - HLRS - NUM - gebert@hlrs.de
-# Created:         27.12.2021
-# Last edit:       28.12.2021
+# Author:    Johannes Gebert - HLRS - NUM - gebert@hlrs.de
+# Created:   27.12.2021
+# Last edit: 23.02.2022
+# ----------------------------------------------------------------------------------------
+SubtreeUrl=git@github.com:biomechanics-hlrs-gebert/A-CESO-Central_Sources.git
 # ----------------------------------------------------------------------------------------
 # Update the subtree
 #
-which git > /dev/null 2> /dev/null
-if  [ $? -ne 0 ] ; then 
+if ! which git > /dev/null 2> /dev/null ; then 
     echo "Git not found."
     echo "The program cannot get updates from this directory."
     echo "The program cannot compile if the »central_src« directory ist missing."
 fi
 #
-ls -l $PWD/central_src > /dev/null 2> /dev/null
-if  [ $? -ne 0 ] ; then 
+if  ! ls -l "$PWD"/central_src > /dev/null 2> /dev/null ; then 
     operation="add"
 else
     operation="pull"
 fi
 #
-git subtree $operation --prefix \
-central_src git@github.com:biomechanics-hlrs-gebert/A-CESO-Central_Sources.git \
-main --squash
+git subtree $operation --prefix central_src $SubtreeUrl main --squash
