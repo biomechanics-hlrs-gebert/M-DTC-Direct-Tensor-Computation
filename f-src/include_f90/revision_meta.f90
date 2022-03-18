2ee5a772edb1fda44d4827362b26be8dc7be74fc
JoGebert <johannes.gebert@hlrs.de>
2022-03-18 19:07:30 +0100

    Squashed 'geb-lib/' changes from 33bfae3..28adc93
    28adc93 added .gitkeep to preserve c_objects directory
    35b9f72 Merge pull request #1 from biomechanics-hlrs-gebert/port_meta_to_c
    4f29402 fixed error in makefile (wrong fortran compiler)
    0c768d5 integrated c source compiling in makefile, small additions to mod_meta.c
    8bfb951 Initial commit of meta implementation in c
    feea4bc quicksafe
    9563af6 Citation added
    9991cb9 Update README.md
    01d9951 Create LICENSE
    b142a32 Enhanced title
    5f5952a Restructuring
    
    git-subtree-dir: geb-lib
    git-subtree-split: 28adc93f7268c0357beae59da4ba3fa9ad327b3b

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'Directly Discretizing Tensor Computation'
-CHARACTER(LEN=scl), PARAMETER :: hash = '0142455330c458b5987534304630940fc3a96e86'
+CHARACTER(LEN=scl), PARAMETER :: longname = 'Direct Tensor Computation'
+CHARACTER(LEN=scl), PARAMETER :: hash = '05c4a8009f9000419e1e89fbd61665ec58d0aa09'
