b6985c9b3767f7881356f30b7fd9ef33a9c68854
JoGebert <johannes.gebert@hlrs.de>
2022-03-20 23:09:47 +0100

    Subtree refactored

--- central_src/f-src/include_f90/revision_meta.f90
+++
@@ -1,2 +0,0 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'Direct Tensor Computation'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'c5f2811584c4f88f11c4241711cb88ad57e1aba6'
--- geb-lib/f-src/include_f90/revision_meta.f90
+++ geb-lib/f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'Direct Tensor Computation'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'bccb69ff5e1689f332a50d2b30bfb261caa5b432'
+CHARACTER(LEN=scl), PARAMETER :: hash = 'ce4cae09ff1b900e68332f7accf7410530ae3934'
