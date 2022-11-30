e9c24fda2bdfe6b7e8818acfd3f1c18d5f55a11e
JoGebert <johannes.gebert@hlrs.de>
2022-11-30 13:10:45 +0100

    Task geometry for ppd < proc_per_node is working

--- auxiliaries/write_task_geometry.DTC.hawk.py
+++ auxiliaries/write_task_geometry.DTC.hawk.py
@@ -27,6 +27,8 @@ master_rank=0                # Depends on the MPI implementation of DTC!
 # -----------------------------------------------------------------------------
 parser = argparse.ArgumentParser(description='Create a file containing the task geometry for an HPE MPT batch job.')
 parser.add_argument('-if', action="store", dest="in_file", help="Input batch file (PBS).")
+parser.add_argument('-nodes', action="store", dest="nodes_available", help="Number of nodes of job submission.")
+parser.add_argument('-np', action="store", dest="processors", help="Number of processors (size_mpi, not the total number of all nodes).")
 parser.add_argument('-tf', action="store", dest="task_file", help="Input batch file (PBS).")
 
 # -----------------------------------------------------------------------------
@@ -47,20 +49,33 @@ if not os.path.exists(cmd_args.in_file):
 
 if not bool(cmd_args.task_file):
     print("Please specify an input file.")
+    exit(1)
+
+if os.path.exists(cmd_args.task_file):
+    print("The task_geometry file " + cmd_args.task_file + " already exists.")
+    exit(2)
+
+if not bool(cmd_args.nodes_available):
+    print("Please specify the number of nodes available.")
     exit(3)
 
-if not os.path.exists(cmd_args.task_file):
-    print("Input file »" + cmd_args.task_file + "« does not exist.")
-    exit(4)
+if not bool(cmd_args.processors):
+    print("Please specify the number of processors used.")
+    exit(3)
+else:
+    try:
+        size_mpi=int(cmd_args.processors.strip())
 
-
+        print( "MM Number of processors " + cmd_args.in_file + "=" + str(size_mpi))
+    except:
+        pass
+        
 # -----------------------------------------------------------------------------
 # Parse the batch file.
 # -----------------------------------------------------------------------------
 with open(cmd_args.in_file) as file:
     lines = [line.rstrip() for line in file]
 
-    size_mpi=0
     ppd=0
     parse_keywords = False
     
@@ -69,13 +84,16 @@ with open(cmd_args.in_file) as file:
         # -----------------------------------------------------------------------------
         # Parse the node ID.
         # -----------------------------------------------------------------------------
-        keyword = int(line[2:23])
+        keyword = line[2:23]
 
         # -----------------------------------------------------------------------------
         # Stop parsing if the meta file contains another program.
         # -----------------------------------------------------------------------------
-        if int(line[0]) == "p" and parse_keywords:
-            parse_keywords = False
+        try:
+            if line[0] == "p" and parse_keywords:
+                parse_keywords = False
+        except:
+            pass
 
         # -----------------------------------------------------------------------------
         # Start parsing if the keywords belong to DTC.
@@ -83,28 +101,22 @@ with open(cmd_args.in_file) as file:
         if keyword.strip() == "TENSOR-COMPUTATION":
             parse_keywords = True
 
-        if keyword.strip() == "PROCESSORS" and parse_keywords:
-            clipboard=line[24:65]
-
-            size_mpi=int(clipboard.strip())
-
-            print( "MM PROCESSORS of " + cmd_args.in_file + "=" + str(size_mpi) + "\n")
-
-
         if keyword.strip() == "MESH_PER_SUB_DMN" and parse_keywords:
-            clipboard=line[24:65]
+            try:
+                clipboard=line[24:65]
 
-            ppd=int(clipboard.strip())
+                ppd=int(clipboard.strip())
 
-            print( "MM MESH_PER_SUB_DMN of " + cmd_args.in_file + "=" + str(ppd) + "\n")
-       
+                print( "MM MESH_PER_SUB_DMN of " + cmd_args.in_file + "=" + str(ppd))
+            except:
+                pass
 
     # -----------------------------------------------------------------------------
     # Check presence of the variables
     # -----------------------------------------------------------------------------
     if size_mpi == 0:
-        print ("EE Keyword PROCESSORS not parsed properly.")
-        exit(6)
+        print ("Number of processors wrong.")
+        exit(4)
     
     if ppd == 0:
         print ("EE Keyword MESH_PER_SUB_DMN not parsed properly.")
@@ -132,10 +144,19 @@ if ppd > processors_per_node:
 
 else:
     no_nodes_per_domain = 1
+    overhead = 0
+    processors_per_node=ppd
 
-parallel_domains = (size_mpi - 1)/ppd
+parallel_domains = int((size_mpi - 1)/ppd)
 
 total_no_nodes = (no_nodes_per_domain * parallel_domains) + 1
+
+if int(cmd_args.nodes_available) > total_no_nodes:
+    print ("""
+There are more MPI processes and nodes requested then available.
+This will lead to an undefined state and is not suitable for power measurements.
+Input file: """ + cmd_args.in_file)
+    exit(10)
 
 # -----------------------------------------------------------------------------
 # Concatenate the task geometry like {(0)(1,2,5)(4)}:
@@ -147,42 +168,55 @@ task_geom_string="{(" + str(master_rank) + ")"
 
 current_rank = master_rank + 1
 
-for ii in range(1, parallel_domains):
+print ("ppd: " + str(ppd))
+print ("parallel_domains: " + str(parallel_domains))
+print ("no_nodes_per_domain: " + str(no_nodes_per_domain))
+print ("processors_per_node: " + str(processors_per_node))
+print ("size_mpi: " + str(size_mpi))
+
+for ii in range(0, parallel_domains):
 
     current_overhead = 0
-    current_proc_per_domain = 1
+
+    for jj in range(0, no_nodes_per_domain):
     
-    for jj in range(1, no_nodes_per_domain):
+        current_proc_per_node = 1
 
         substring = "("
-        
-        for kk in range(1, processors_per_node):
 
-            substring = substring + str(current_rank) 
+        for kk in range(0, processors_per_node):
 
-            if current_proc_per_domain < processors_per_node:
-                substring = substring + ","
-            
-            current_rank += 1
-            current_proc_per_domain += 1
+            substring = substring
 
-        # -----------------------------------------------------------------------------
-        # Write overhead if required.
-        # -----------------------------------------------------------------------------
-        if current_overhead < overhead:
-
-            substring = substring + str(current_rank)
-            current_overhead += 1
+            if current_proc_per_node < ppd:
+                substring = substring + str(current_rank) + ","
+            else:
+                substring = substring + str(current_rank) 
 
             current_rank += 1
-            current_proc_per_domain += 1
+            current_proc_per_node += 1
+
+
+            # -----------------------------------------------------------------------------
+            # Write overhead if required.
+            # -----------------------------------------------------------------------------
+            if current_overhead < overhead:
+
+                substring = substring + "," + str(current_rank)
+                current_overhead += 1
+
+                current_rank += 1
+                current_proc_per_node += 1
 
         # -----------------------------------------------------------------------------
         # Finish node entry
         # -----------------------------------------------------------------------------
         substring = substring + ")"
 
+        print(substring)
+
         task_geom_string = task_geom_string + substring
+
 
 
 # -----------------------------------------------------------------------------
