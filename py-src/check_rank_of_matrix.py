a833b59b24f3929ade18db71141dd693f0a9e100
JoGebert <johannes.gebert@hlrs.de>
2023-02-19 12:24:29 +0100

    Determinant V1.0

--- py-src/check_rank_of_matrix.py
+++ py-src/check_rank_of_matrix.py
@@ -86,6 +86,7 @@ def init_displ_hexe20() :
     uu = np.zeros((20,3,60))
     z = 0.
     eps = 0.000001
+    f  = 0.99794 
 
   # uu[ Knoten (1-20), dof (1-3), Load Case (1-60)]
   # uu[:,1,1]    = np.array([       1,       2,       3,       4,       5,       6,       7,       8,            9,      10,      11,      12,      13,      14,      15,      16,      17,      18,      19,      20 ])
@@ -128,36 +129,36 @@ def init_displ_hexe20() :
 
     # Node 9                                                                                                                                                                                                             # independent Load Case | total Load Case
     uu[:,1,24]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,          eps,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  1 | tLC 25
-    uu[:,2,24]   = np.array([   2*eps,       z,       z,       z,       z,       z,       z,       z,          eps,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  1 | tLC 25
+    uu[:,2,24]   = np.array([       z,    -eps,       z,       z,       z,       z,       z,       z,          eps,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  1 | tLC 25
     # Node 10
     uu[:,0,25]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,    -eps,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  2 | tLC 26
     uu[:,2,25]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,     eps,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  2 | tLC 26
     # Node 11
     uu[:,1,26]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,    -eps,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  3 | tLC 27
-    uu[:,2,26]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,     eps,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  3 | tLC 27
+    uu[:,2,26]   = np.array([       z,       z,     eps,       z,       z,       z,       z,       z,            z,       z,     eps,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  3 | tLC 27
     # Node 12
     uu[:,0,27]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,     eps,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  4 | tLC 28
-    uu[:,2,27]   = np.array([       z,       z,   2*eps,       z,       z,       z,       z,       z,            z,       z,       z,     eps,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  4 | tLC 28
+    uu[:,2,27]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,     eps,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  4 | tLC 28
     # Node 13
     uu[:,1,28]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,     eps,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  5 | tLC 29
     uu[:,2,28]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,    -eps,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  5 | tLC 29
     # Node 14
     uu[:,0,29]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  6 | tLC 30
-    uu[:,2,29]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  6 | tLC 30
+    uu[:,2,29]   = np.array([       z,       z,       z,       z,       z,   2*eps,       z,       z,            z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC  6 | tLC 30
     # Node 15
     uu[:,1,30]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z,       z ]) # HEXE20 iLC  7 | tLC 31
-    uu[:,2,30]   = np.array([       z,       z,       z,       z,       z,       z,       z,   3*eps,            z,       z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z,       z ]) # HEXE20 iLC  7 | tLC 31
+    uu[:,2,30]   = np.array([       z,       z,   4*eps,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z,       z ]) # HEXE20 iLC  7 | tLC 31
     # Node 16
     uu[:,0,31]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,     eps,       z,       z,       z,       z ]) # HEXE20 iLC  8 | tLC 32
     uu[:,2,31]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z ]) # HEXE20 iLC  8 | tLC 32
     # Node 17
-    uu[:,0,32]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,     eps,       z,       z,       z ]) # HEXE20 iLC  9 | tLC 33
+    uu[:,0,32]   = np.array([       z,       z,       z,       z,       z,     eps,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,     eps,       z,       z,       z ]) # HEXE20 iLC  9 | tLC 33
     uu[:,1,32]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,     eps,       z,       z,       z ]) # HEXE20 iLC  9 | tLC 33
     # Node 18
-    uu[:,0,33]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,       z,    -eps,       z,       z ]) # HEXE20 iLC 10 | tLC 34
-    uu[:,1,33]   = np.array([       z,       z,       z,       z,  -2*eps,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,       z,     eps,       z,       z ]) # HEXE20 iLC 10 | tLC 34
+    uu[:,0,33]   = np.array([       z,       z,       z,       z,       z,     eps,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,       z,    -eps,       z,       z ]) # HEXE20 iLC 10 | tLC 34
+    uu[:,1,33]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,       z,     eps,       z,       z ]) # HEXE20 iLC 10 | tLC 34
     # Node 19
-    uu[:,0,34]   = np.array([       z,       z,       z,       z,       z,   2*eps,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,       z,       z,    -eps,       z ]) # HEXE20 iLC 11 | tLC 35
+    uu[:,0,34]   = np.array([       z,       z,       z,       z,       z,     eps,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,       z,       z,    -eps,       z ]) # HEXE20 iLC 11 | tLC 35
     uu[:,1,34]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,       z,       z,    -eps,       z ]) # HEXE20 iLC 11 | tLC 35
     # Node 20
     uu[:,0,35]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z,     eps ]) # HEXE20 iLC 12 | tLC 36
@@ -173,7 +174,7 @@ def init_displ_hexe20() :
     uu[:,1,43]   = np.array([       z,       z,       z,       z,       z,       z,       z,    -eps,            z,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC 20 | tLC 44 | Single Vertex
 
     uu[:,1,44]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,     eps,     eps,    -eps,    -eps ]) # HEXE20 iLC 21 | tLC 45 | Compress Center
-    uu[:,2,45]   = np.array([       z,       z,       z,   2*eps,       z,       z,       z,       z,            z,     eps,       z,     eps,       z,    -eps,       z,    -eps,       z,       z,       z,       z ]) # HEXE20 iLC 22 | tLC 46 | Compress Center
+    uu[:,2,45]   = np.array([       z,       z,       z,   3*eps,       z,     eps,       z,       z,            z,     eps,       z,     eps,       z,    -eps,       z,    -eps,       z,       z,       z,       z ]) # HEXE20 iLC 22 | tLC 46 | Compress Center
     uu[:,0,46]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,       z,     eps,    -eps,    -eps,     eps ]) # HEXE20 iLC 23 | tLC 47 | Compress Center
     uu[:,2,47]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,          eps,       z,     eps,       z,    -eps,       z,    -eps,       z,       z,       z,       z,       z ]) # HEXE20 iLC 24 | tLC 48 | Compress Center
     uu[:,0,48]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,    -eps,       z,     eps,       z,    -eps,       z,     eps,       z,       z,       z,       z ]) # HEXE20 iLC 25 | tLC 49 | Compress Center
@@ -183,7 +184,7 @@ def init_displ_hexe20() :
     uu[:,1,51]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,     eps,       z,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC 28 | tLC 52 | Move Center
     uu[:,0,52]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,    -eps,       z,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC 29 | tLC 53 | Move Center
     uu[:,1,53]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,    -eps,       z,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC 30 | tLC 54 | Move Center
-    uu[:,0,54]   = np.array([       z,       z,       z,  -4*eps,       z,       z,       z,       z,            z,       z,       z,       z,     eps,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC 31 | tLC 55 | Move Center
+    uu[:,0,54]   = np.array([       z,       z,       z, f*2*eps,       z,       z,       z,       z,            z,       z,       z,       z,     eps,       z,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC 31 | tLC 55 | Move Center
     uu[:,1,55]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,     eps,       z,       z,       z,       z,       z,       z ]) # HEXE20 iLC 32 | tLC 56 | Move Center
     uu[:,0,56]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z,       z ]) # HEXE20 iLC 33 | tLC 57 | Move Center
     uu[:,0,57]   = np.array([       z,       z,       z,       z,       z,       z,       z,       z,            z,       z,       z,       z,       z,       z,       z,    -eps,       z,       z,       z,       z ]) # HEXE20 iLC 34 | tLC 58 | Move Center
@@ -204,8 +205,19 @@ Rank = np.linalg.matrix_rank(UU, tol=None, hermitian=False)
 # Print HEXE8 displacements
 #------------------------------------------------------------------------------  
 UU *= 1000000
+print("Scaled UU:")
 print(np.around(UU.astype(int)))
 
+#------------------------------------------------------------------------------  
+# Diagonalize HEXE8 UU
+#------------------------------------------------------------------------------  
+# DD = np.diag(np.diag(UU))
+# print("Diagonalized:")
+# print(np.around(DD.astype(int)))
+
+#------------------------------------------------------------------------------  
+# Get determinant
+#------------------------------------------------------------------------------  
 det = np.linalg.det(UU)
 
 print("Rank        of HEXE8 displacements:", Rank)
@@ -224,12 +236,19 @@ UU = assemble_UU(uu, 20, 3)
 
 Rank = np.linalg.matrix_rank(UU, tol=None, hermitian=False)
 
-
 #------------------------------------------------------------------------------  
 # Print HEXE20 displacements
 #------------------------------------------------------------------------------  
 UU *= 1000000
+print("Scaled UU:")
 print(np.around(UU.astype(int)))
+
+#------------------------------------------------------------------------------  
+# Diagonalize HEXE20 UU
+#------------------------------------------------------------------------------  
+# DD = np.diag(np.diag(UU))
+# print("Diagonalized:")
+# print(DD)
 
 #------------------------------------------------------------------------------  
 # Get determinant
